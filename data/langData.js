b66ca4eccfdcfd1901c7f8dc1b0d0d2f6d26a99b
Ayberk Yiğit <kurtknispel9@gmail.com>
2023-02-22 18:26:13 +0300

    Created an instance for i18n via sign-up page

---
+++ data/langData.js
@@ -0,0 +1,16 @@
+export const message = {
+    en:{
+      heading:"Heading",
+      heading_main_menu:"Demo Main Menu",
+      title:"Title",
+      signup:"Sign Up",
+      footer:"Footer",
+    },
+    tr:{
+      heading:"Başlık",
+      heading_main_menu:"Demo Anasayfa",
+      title:"Başlık",
+      signup:"Üye OL",
+      footer:"Alt kısım"
+    }
+  }
